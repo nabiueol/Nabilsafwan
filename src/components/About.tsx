88904b4aabec05934d557465a5687f019560bba1
Nabe <nabiueol@gmail.com>
2025-06-08 06:46:43 +0800

    fix error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from './Hero'
+// import Hero from './Hero'
 import Link from 'next/link';
 import { FaInstagram } from "react-icons/fa6";
 import { FaTiktok } from "react-icons/fa";
